a1fe55f9137314d5c9758a65776e31efbd6d8089
lidneup <phyahz@gmail.com>
2021-11-01 16:59:10 -0100

    build: add build folder to .gitignore

--- test/cmake-build-release/vendor/google_test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lidneup/repos/8086_emulator/test/vendor/google_test
-# Build directory: /home/lidneup/repos/8086_emulator/test/cmake-build-release/vendor/google_test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googletest")
--- test/cmake-build-release/vendor/google_test/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lidneup/repos/8086_emulator/test/vendor/google_test/googletest
-# Build directory: /home/lidneup/repos/8086_emulator/test/cmake-build-release/vendor/google_test/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
